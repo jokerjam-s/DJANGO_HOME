f8e5ec3da9098ab66b79f5beb57b2dab5a23a5d1
Андрей Сапунов <jokerjam@yandex.ru>
2023-10-12 16:41:34 +0300

    work 6 in progress

--- django_first/django_first/settings.py
+++ django_first/django_first/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,14 +19,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-%4vid^3lm!_!((avoa&)&e@%10iy&f6(8kc(j^@ayj($l$4ur3"
+# SECRET_KEY = "django-insecure-%4vid^3lm!_!((avoa&)&e@%10iy&f6(8kc(j^@ayj($l$4ur3"
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
-    "192.168.0.100",
+    'jokerjam.pythonanywhere.com',
 ]
 
 # Application definition
@@ -75,12 +79,21 @@ WSGI_APPLICATION = "django_first.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'jokerjam$default',
+        'USER': 'jokerjam',
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'jokerjam.mysql.pythonanywhere-services.com',
+        'OPTIONS': {
+            'init_command': "SET NAMES 'utf8mb4'; SET sql_mode = 'STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
+    },
 }
+
 
 # Media
 MEDIA_URL = '/media/'
